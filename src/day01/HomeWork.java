e0f4075fccd66cd4253eb957b4b831f058f03856
cuizr <34389446+cuizr@users.noreply.github.com>
2020-06-17 09:28:34 +0800

    Update HomeWork.java

--- src/day01/HomeWork.java
+++ src/day01/HomeWork.java
@@ -3,5 +3,8 @@ package day01;
 public class HomeWork {
     public static void main(String[] args) {
         System.out.println("成功");
+        System.out.println("成功");
+        System.out.println("成功");
+        
     }
 }
