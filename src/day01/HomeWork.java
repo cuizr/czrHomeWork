c5df244559e594aa35f323ed7ba70c17fffa54d2
cuizr <34389446+cuizr@users.noreply.github.com>
2020-06-17 20:45:22 +0800

    Update HomeWork.java

--- src/day01/HomeWork.java
+++ src/day01/HomeWork.java
@@ -10,7 +10,7 @@ public class HomeWork {
     public static void triangle(){
         int n=4;
         for (int i=0;i<=n;i++){
-            for (int j=1;j<=n;j++){
+            for (int j=1;j<=n-i;j++){
                 System.out.print(" ");
             }
             for (int k=1;k<=2*i-1;k++){
