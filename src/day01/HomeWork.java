a298e445464c28d890964efbcc47abe509899cd6
cuizr <cuizr8023@live.com>
2020-06-17 16:27:30 +0800

    崔峥嵘的第一次作业

--- src/day01/HomeWork.java
+++ src/day01/HomeWork.java
@@ -2,9 +2,38 @@ package day01;
 
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println("成功");
-        System.out.println("成功");
-        System.out.println("成功");
-        
+
+        triangle();
+        primeSum();
+    }
+//等腰三角形
+    public static void triangle(){
+        int n=4;
+        for (int i=0;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                System.out.println(" ");
+            }
+            for (int k=1;k<=2*i-1;k++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+    //100内质数的和
+    public static void primeSum(){
+        int sum=0;
+        for (int i=2 ;i<=100;i++){
+            boolean bool = true;
+            for (int j=2 ; j<=Math.sqrt(i);j++){
+                if(i%j==0){
+                    bool =false;
+                    break;
+                }
+            }
+            if (bool == true){
+                sum+=i;
+            }
+        }
+        System.out.println("1-100的质数和为"+sum);
     }
 }
